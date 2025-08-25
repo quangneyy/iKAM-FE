2e297cfb671dab44a7e3069df75711253b2aa01d
quangneyy <quangney12602@gmail.com>
2025-08-25 15:31:53 +0700

    12-commit

--- next.config.ts
+++ next.config.ts
@@ -2,20 +2,5 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async redirects() {
-    return [
-      {
-        source: "/vi",
-        destination: "/",
-        permanent: false,
-      },
-      {
-        source: "/vi/:path*",
-        destination: "/:path*",
-        permanent: false,
-      },
-    ];
-  },
 };
-
 export default nextConfig;
